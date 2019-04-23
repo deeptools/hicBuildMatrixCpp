5ff3dff374ee2d6ab595955936aab17de163962f
Klesta Toti <ktoti@tf.uni-freiburg.de>
2019-04-23 12:04:35 +0200

    Constructing a function to check dublications

--- src/hicbuildmatrix.hpp
+++ src/hicbuildmatrix.hpp
@@ -1,27 +1,27 @@
 #ifndef HICBUILDMATRIX_HPP
 #define HICBUILDMATRIX_HPP
 
-// #include <omp.h>
-// #include <seqan/file.h>
-// using namespace seqan;
-// #include <iostream>
-
 #include <iostream>
 #include <vector> 
 #include <string>
 #include <seqan/bam_io.h>
 #include <seqan/sequence.h>
 #include <seqan/seq_io.h>
+#include <assert.h> 
 
 class HiCBuildMatrix
 {
   public:
     HiCBuildMatrix(const std::string &pForwardRead, const std::string &pReverseRead);
     size_t readBamFile();
-
+    bool is_duplicacted(chrom1, start1, chrom2, start2); 
   private:
     std::string mForwardRead;
     std::string mReverseRead;
+    std::string chrom1;
+    std::string chrom2;
+    std::int start1;;
+    std::int start2;
 
 };
 
