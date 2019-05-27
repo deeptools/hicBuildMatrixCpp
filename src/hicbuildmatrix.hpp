b083ec01a2f80f80bd2c672d0d926cf9c30134d1
Klesta Toti <ktoti@tf.uni-freiburg.de>
2019-05-27 18:04:54 +0200

    Debugging

--- src/hicbuildmatrix.hpp
+++ src/hicbuildmatrix.hpp
@@ -17,11 +17,12 @@ class HiCBuildMatrix
 {
   public:
     HiCBuildMatrix(const std::string &pForwardRead, const std::string &pReverseRead);
-    size_t readBamFile(int pNumberOfItemsPerBuffer, bool pSkipDuplicationCheck, std::vector<int> &pRefId2name);
+    size_t readBamFile(int pNumberOfItemsPerBuffer, bool pSkipDuplicationCheck, std::vector<std::string> pRefId2name);
 
-    bool is_duplicacted(const std::string &pchrom1,const int pstart1,const std::string &pchrom2,const int &pstart2); 
+    bool is_duplicated(const std::string pchrom1,const int pstart1,const std::string pchrom2,const int pstart2); 
     std::unordered_map<std::string, IntervalTree<size_t, size_t> > createInitialStructures(seqan::BamStream pBamStream, int pBinSize);
-  private:
+    std::vector<std::string> createRefId2name(seqan::BamStream pBamStream);
+  private: 
     std::string mForwardRead;
     std::string mReverseRead;
     std::string mchrom1;
@@ -30,6 +31,7 @@ class HiCBuildMatrix
     int mstart2;
     int mNumberOfItemsPerBuffer;
     bool mSkipDublicationCheck;
+    std::set<std::string> mPosMatrix;
 };
 
 #endif
